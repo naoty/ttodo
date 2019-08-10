edd981483ecd453849b0af2ff3d0b02b8b7368af
Naoto Kaneko <naoty.k@gmail.com>
2019-08-10 12:51:43 +0900

    Display TODOs on table

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/naoty/ttodo
 
 go 1.12
 
-require github.com/rivo/tview v0.0.0-20190721135419-23dc8a0944e4
+require (
+	github.com/gdamore/tcell v1.1.2
+	github.com/rivo/tview v0.0.0-20190721135419-23dc8a0944e4
+)
--- main.go
+++ main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"github.com/gdamore/tcell"
 	"github.com/rivo/tview"
 )
 
 func main() {
-	box := tview.NewBox().SetBorder(true).SetTitle("Hello, tview!")
-	if err := tview.NewApplication().SetRoot(box, true).Run(); err != nil {
+	// Keep background color as users set
+	tview.Styles.PrimitiveBackgroundColor = tcell.ColorDefault
+
+	app := tview.NewApplication()
+	table := tview.NewTable()
+
+	table.SetCellSimple(0, 0, "[ ]")
+	table.SetCellSimple(0, 1, "2019-08-10")
+	table.SetCellSimple(0, 2, "naoty")
+	table.SetCellSimple(0, 3, "go moduleの使い方を思い出す")
+	table.SetCellSimple(1, 0, "[ ]")
+	table.SetCellSimple(1, 1, "2019-08-10")
+	table.SetCellSimple(1, 2, "naoty")
+	table.SetCellSimple(1, 3, "VSCodeのGoの設定を見直す")
+	table.SetCellSimple(2, 0, "[ ]")
+	table.SetCellSimple(2, 1, "2019-08-11")
+	table.SetCellSimple(2, 2, "naoty")
+	table.SetCellSimple(2, 3, "rivo/tviewの使い方を理解する")
+
+	if err := app.SetRoot(table, true).SetFocus(table).Run(); err != nil {
 		panic(err)
 	}
 }
