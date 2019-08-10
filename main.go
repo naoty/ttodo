b9b4feb80519a98f69d0e4c08ac99704214a1fad
Naoto Kaneko <naoty.k@gmail.com>
2019-08-10 18:11:39 +0900

    Add headers to table

--- main.go
+++ main.go
@@ -11,18 +11,18 @@ func main() {
 		SetSelectable(true, false).
 		SetSelectedStyle(tcell.ColorDefault, tcell.Color100, 0)
 
-	table.SetCellSimple(0, 0, "[ ]")
-	table.SetCellSimple(0, 1, "2019-08-10")
-	table.SetCellSimple(0, 2, "naoty")
-	table.SetCellSimple(0, 3, "go moduleの使い方を思い出す")
+	table.SetCell(0, 0, tview.NewTableCell("Done").SetSelectable(false))
+	table.SetCell(0, 1, tview.NewTableCell("Deadline").SetSelectable(false))
+	table.SetCell(0, 2, tview.NewTableCell("Assignee").SetSelectable(false))
+	table.SetCell(0, 3, tview.NewTableCell("Title").SetSelectable(false))
 	table.SetCellSimple(1, 0, "[ ]")
 	table.SetCellSimple(1, 1, "2019-08-10")
 	table.SetCellSimple(1, 2, "naoty")
-	table.SetCellSimple(1, 3, "VSCodeのGoの設定を見直す")
+	table.SetCellSimple(1, 3, "TODOのリストを表示する")
 	table.SetCellSimple(2, 0, "[ ]")
-	table.SetCellSimple(2, 1, "2019-08-11")
+	table.SetCellSimple(2, 1, "2019-08-10")
 	table.SetCellSimple(2, 2, "naoty")
-	table.SetCellSimple(2, 3, "rivo/tviewの使い方を理解する")
+	table.SetCellSimple(2, 3, "TODOの詳細を表示する")
 
 	if err := app.SetRoot(table, true).SetFocus(table).Run(); err != nil {
 		panic(err)
