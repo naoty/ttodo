17d7f456ba3cb75e0220e4d3a5a4dd115b91817f
Naoto Kaneko <naoty.k@gmail.com>
2019-08-14 21:45:17 +0900

    Display the version of this app when -v or --version is passed

--- main.go
+++ main.go
@@ -1,122 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"os"
-	"path/filepath"
-
-	"github.com/naoty/ttodo/todo"
-	"github.com/naoty/ttodo/views"
-
-	"github.com/gdamore/tcell"
-	"github.com/rivo/tview"
 )
 
+// Version represents the version of this application.
+var Version string
+
 func main() {
-	app := tview.NewApplication()
-
-	// To reset background color of previous selected rows
-	// https://github.com/rivo/tview/issues/270
-	app.SetBeforeDrawFunc(func(s tcell.Screen) bool {
-		s.Clear()
-		return false
-	})
-
-	pages := tview.NewPages()
-	pages.Box.SetBackgroundColor(tcell.ColorDefault)
-
-	dir := os.Getenv("TODO_PATH")
-	if dir == "" {
-		dir = os.Getenv("HOME")
-	}
-	todoPath := filepath.Join(dir, ".todo.json")
-	todos, err := todo.LoadTodos(todoPath)
-
-	if err != nil {
-		panic(err)
-	}
-
-	table := views.NewTable()
-
-	for _, todo := range todos {
-		table.AddTodoRow(todo)
-	}
-
-	flex := tview.NewFlex().
-		SetDirection(tview.FlexRow).
-		AddItem(table, 0, 1, true)
-	flex.SetBackgroundColor(tcell.ColorDefault)
-
-	debug := tview.NewTextView()
-	debug.SetBackgroundColor(tcell.Color60)
-	flex.AddItem(debug, 1, 0, false)
-
-	descriptionView := views.NewDescription()
-
-	table.SetSelectedFunc(func(row, column int) {
-		if descriptionView.IsShown {
-			descriptionView.IsShown = false
-			flex.RemoveItem(descriptionView)
-		} else {
-			descriptionView.IsShown = true
-
-			if row >= 1 && row <= len(todos) {
-				todo := todos[row-1]
-				descriptionView.SetText(todo.Description)
-				flex.AddItem(descriptionView, 0, 1, true)
-			}
+	for _, arg := range os.Args {
+		switch arg {
+		case "-v", "--version":
+			fmt.Println(Version)
+			os.Exit(0)
 		}
-	})
-
-	table.SetSelectionChangedFunc(func(row, column int) {
-		if !descriptionView.IsShown {
-			return
-		}
-
-		if row >= 1 && row <= len(todos) {
-			todo := todos[row-1]
-			descriptionView.SetText(todo.Description)
-		}
-	})
-
-	form := views.NewForm(func(td todo.Todo) {
-		err := todo.SaveTodo(td, todoPath)
-
-		if err != nil {
-			panic(err)
-		}
-
-		todos, err = todo.LoadTodos(todoPath)
-
-		if err != nil {
-			panic(err)
-		}
-
-		table.UpdateTodoRows(todos)
-		pages.SwitchToPage("main")
-	}, func() {
-		pages.SwitchToPage("main")
-	})
-
-	pages.
-		AddPage("main", flex, true, true).
-		AddPage("new", form, true, false)
-
-	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
-		switch event.Rune() {
-		case 'c':
-			pages.SwitchToPage("new")
-			return nil
-		case 'q':
-			app.Stop()
-			return nil
-		default:
-			return event
-		}
-	})
-
-	err = app.SetRoot(pages, true).Run()
-
-	if err != nil {
-		panic(err)
 	}
 }
